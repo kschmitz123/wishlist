6ad1a30a473ef06d746fe9b44d7f686c0b88a4a8
kschmitz123 <kathrin_schmitz93@gmx.de>
2021-01-12 13:47:49 +0100

    Take over app setup from old project

---
+++ client/src/api/lists.js
@@ -0,0 +1,33 @@
+export async function getLists() {
+  const response = await fetch("http://localhost:5000/lists");
+  const lists = await response.json();
+  return lists;
+}
+export async function getListById(id) {
+  const response = await fetch(`http://localhost:5000/lists/${id}`);
+  const list = await response.json();
+  return list;
+}
+export async function deleteListById(id) {
+  await fetch(`http://localhost:5000/lists/${id}`, {
+    method: "DELETE",
+  });
+}
+export async function postList(title) {
+  const response = await fetch(`http://localhost:5000/lists/`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(title),
+  });
+  const newList = await response.json();
+  return newList;
+}
+export async function patchListItem(id, wish) {
+  await fetch(`http://localhost:5000/lists/${id}`, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      wishes: wish,
+    }),
+  });
+}
---
+++ client/src/components/Button.js
@@ -0,0 +1,20 @@
+import styled from "styled-components/macro";
+
+const FloatingActionButton = styled.button`
+  position: fixed;
+  bottom: 10px;
+  right: 10px;
+  display: inline-grid;
+  place-content: center;
+  text-decoration: none;
+  height: 60px;
+  width: 60px;
+  background-color: #ff9a9e;
+  border: none;
+  border-radius: 50%;
+  cursor: pointer;
+  &:visited {
+    color: black;
+  }
+`;
+export default FloatingActionButton;
