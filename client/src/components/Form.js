c08d73171a35a127d77494cb34be73557f9a4db5
kschmitz123 <kathrin_schmitz93@gmx.de>
2021-01-13 10:21:16 +0100

    Add styling to button

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -3,12 +3,18 @@ import styled from "styled-components/macro";
 const Form = styled.form`
   display: flex;
   flex-direction: column;
+  padding-top: 20px;
 
-  input {
+  input,
+  button {
     height: 2.5rem;
     border-radius: 25px;
     margin-bottom: 10px;
     border: none;
+    text-align: center;
+  }
+  button {
+    background: #e48585;
   }
 `;
 export default Form;
