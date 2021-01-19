2eb1f4c3369a1d27555e20c52ab8dee403c4e67b
kschmitz123 <kathrin_schmitz93@gmx.de>
2021-01-19 16:23:10 +0100

    Add theme provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,35 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import WishList from "./pages/WishList";
-
-import GlobalStyle from "./GlobalStyle";
+import GlobalStyle, { theme } from "./GlobalStyle";
 import Add from "./pages/Add";
 import Welcome from "./pages/Welcome";
 import React from "react";
+import { ThemeProvider } from "styled-components";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router>
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
         <GlobalStyle />
-        <Switch>
-          <Route exact path="/">
-            <Welcome />
-          </Route>
-          <Route path="/add">
-            <Add />
-          </Route>
-          <Route path="/wishlist/:listId">
-            <WishList />
-          </Route>
-        </Switch>
-      </Router>
-    </QueryClientProvider>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Welcome />
+            </Route>
+            <Route path="/add">
+              <Add />
+            </Route>
+
+            <Route path="/wishlist/:listId">
+              <WishList />
+            </Route>
+          </Switch>
+        </Router>
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 }
 
