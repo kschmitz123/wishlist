d2d78f55b06fe3d7207277c75e188d52467d3a58
kschmitz123 <kathrin_schmitz93@gmx.de>
2021-01-20 09:42:06 +0100

    Remove use effect and import lists

--- client/src/pages/WishList.js
+++ client/src/pages/WishList.js
@@ -1,36 +1,24 @@
+import React, { useEffect, useState } from "react";
+import { useMutation, useQuery } from "react-query";
 import { Link, useHistory, useParams } from "react-router-dom";
-import React, { useState, useEffect } from "react";
-import { useQuery, useMutation } from "react-query";
-import FloatingActionButton from "../components/Button";
-import Wrapper from "../components/Wrapper";
-import { getListById, deleteListById, patchListItem } from "../api/lists";
-import WishListItem from "../components/WishListItem";
+import { deleteListById, getListById, patchListItem } from "../api/lists";
 import BackArrow from "../assets/back-arrow.png";
+import FloatingActionButton from "../components/Button";
 import DangerButton from "../components/DangerButton";
 import ErrorMessage from "../components/ErrorMessage";
 import Form from "../components/Form";
+import WishListItem from "../components/WishListItem";
 import { theme } from "../GlobalStyle";
-import Container from "../components/Container";
+import BirthdayList from "./BirthdayList";
+import ChristmasList from "./ChristmasList";
 
 const WishList = () => {
   const { listId } = useParams();
   const history = useHistory();
-  const [background, setBackground] = useState("");
-  const [backgroundColor, setBackgroundColor] = useState("");
   const [wishToAdd, setWishToAdd] = useState("");
   const { data, status } = useQuery(["lists", listId], () =>
     getListById(listId)
   );
-
-  useEffect(() => {
-    if (data?.theme === "christmas") {
-      setBackground(theme.background.christmas);
-      setBackgroundColor(theme.backgroundColor.christmas);
-    } else {
-      setBackground(theme.background.birthday);
-      setBackgroundColor(theme.backgroundColor.birthday);
-    }
-  }, [data]);
 
   const mutation = useMutation(() => {
     patchListItem({
@@ -51,40 +39,69 @@ const WishList = () => {
   };
 
   return (
-    <Wrapper
-      style={{
-        background: background,
-      }}
-    >
-      <Container style={{ backgroundColor: backgroundColor }}>
-        {status === "loading" && <div>Loading...</div>}
-        {status === "error" && <ErrorMessage>Error fetching list</ErrorMessage>}
-
-        <h1>Wishlist for: {data?.name}</h1>
-        <p>
-          {data?.wishes.map((wish, index) => (
-            <WishListItem key={index} title={wish} />
-          ))}
-        </p>
-        <Form onSubmit={handleSubmit}>
-          <input
-            placeholder="Add wish"
-            type="text"
-            onChange={handleChange}
-            required
-          />
-          <button type="submit">Add</button>
-        </Form>
-        <DangerButton onClick={handleDelete} type="button">
-          Delete
-        </DangerButton>
-        <Link to="/">
-          <FloatingActionButton>
-            <img src={BackArrow} alt="back" />
-          </FloatingActionButton>
-        </Link>
-      </Container>
-    </Wrapper>
+    <>
+      {data && data.theme === "christmas" ? (
+        <ChristmasList>
+          {status === "loading" && <div>Loading...</div>}
+          {status === "error" && (
+            <ErrorMessage>Error fetching list</ErrorMessage>
+          )}
+          <h1>Wishlist for: {data?.name}</h1>
+          <p>
+            {data?.wishes.map((wish, index) => (
+              <WishListItem key={index} title={wish} />
+            ))}
+          </p>
+          <Form onSubmit={handleSubmit}>
+            <input
+              placeholder="Add wish"
+              type="text"
+              onChange={handleChange}
+              required
+            />
+            <button type="submit">Add</button>
+          </Form>
+          <DangerButton onClick={handleDelete} type="button">
+            Delete
+          </DangerButton>
+          <Link to="/">
+            <FloatingActionButton>
+              <img src={BackArrow} alt="back" />
+            </FloatingActionButton>
+          </Link>
+        </ChristmasList>
+      ) : (
+        <BirthdayList>
+          {status === "loading" && <div>Loading...</div>}
+          {status === "error" && (
+            <ErrorMessage>Error fetching list</ErrorMessage>
+          )}
+          <h1>Wishlist for: {data?.name}</h1>
+          <p>
+            {data?.wishes.map((wish, index) => (
+              <WishListItem key={index} title={wish} />
+            ))}
+          </p>
+          <Form onSubmit={handleSubmit}>
+            <input
+              placeholder="Add wish"
+              type="text"
+              onChange={handleChange}
+              required
+            />
+            <button type="submit">Add</button>
+          </Form>
+          <DangerButton onClick={handleDelete} type="button">
+            Delete
+          </DangerButton>
+          <Link to="/">
+            <FloatingActionButton>
+              <img src={BackArrow} alt="back" />
+            </FloatingActionButton>
+          </Link>
+        </BirthdayList>
+      )}
+    </>
   );
 };
 export default WishList;
