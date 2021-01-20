9af6d8d1c94cf811dd30d9226387b686fe66756f
kschmitz123 <kathrin_schmitz93@gmx.de>
2021-01-20 09:42:49 +0100

    Remove width from wrapper

--- client/src/components/Wrapper.js
+++ client/src/components/Wrapper.js
@@ -3,7 +3,6 @@ import styled from "styled-components/macro";
 const Wrapper = styled.div`
   padding: 30px;
   text-align: center;
-  height: 100vh;
-  width: 100vw;
+  height: 100vh; ;
 `;
 export default Wrapper;
