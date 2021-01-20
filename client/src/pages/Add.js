1e00d428c16ecede17155bd257f4d4cb2384533d
kschmitz123 <kathrin_schmitz93@gmx.de>
2021-01-20 10:00:41 +0100

    Add header to add page & remove unused code

--- client/src/pages/Add.js
+++ client/src/pages/Add.js
@@ -1,13 +1,13 @@
-import { Link, useHistory } from "react-router-dom";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { useMutation } from "react-query";
+import { Link, useHistory } from "react-router-dom";
+import { postList } from "../api/lists";
+import BackArrow from "../assets/back-arrow.png";
 import FloatingActionButton from "../components/Button";
 import Form from "../components/Form";
+import Header from "../components/Header";
 import Wrapper from "../components/Wrapper";
-import BackArrow from "../assets/back-arrow.png";
-import { useMutation } from "react-query";
-import { postList } from "../api/lists";
-import { useForm } from "react-hook-form";
-import { theme } from "../GlobalStyle";
 
 const Add = () => {
   const { register, handleSubmit } = useForm();
@@ -23,7 +23,6 @@ const Add = () => {
   const onSubmit = async (data) => {
     const wishes = data.wishes.split(",");
     const { title, theme } = data;
-    console.log(theme);
     try {
       const newList = await mutation.mutateAsync({ title, wishes, theme });
       history.push(`/wishlist/${newList}`);
@@ -32,36 +31,41 @@ const Add = () => {
     }
   };
   return (
-    <Wrapper style={{ background: theme.background.standard }}>
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          placeholder="Add name"
-          type="text"
-          required
-          ref={register}
-          name="title"
-        />
-        <input
-          type="text"
-          placeholder="Insert Wishes here: Wish 1, Wish 2, ..."
-          ref={register}
-          name="wishes"
-        />
-        <select ref={register} name="theme" aria-label={"Select theme"}>
-          <option value="">--Please choose a theme--</option>
-          <option value="christmas">Christmas</option>
-          <option value="birthday">Birthday</option>
-        </select>
-        <button type="submit">Add</button>
-      </Form>
-      {mutation.isLoading && <div>Loading...</div>}
-      {mutation.isError && <p>{mutation.error.message}</p>}
-      <Link to="/">
-        <FloatingActionButton>
-          <img src={BackArrow} alt="back" />
-        </FloatingActionButton>
-      </Link>
-    </Wrapper>
+    <>
+      <Header>
+        <h1>Add a new list</h1>
+      </Header>
+      <Wrapper>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            placeholder="Add name"
+            type="text"
+            required
+            ref={register}
+            name="title"
+          />
+          <input
+            type="text"
+            placeholder="Insert Wishes here: Wish 1, Wish 2, ..."
+            ref={register}
+            name="wishes"
+          />
+          <select ref={register} name="theme" aria-label={"Select theme"}>
+            <option value="">--Please choose a theme--</option>
+            <option value="christmas">Christmas</option>
+            <option value="birthday">Birthday</option>
+          </select>
+          <button type="submit">Add</button>
+        </Form>
+        {mutation.isLoading && <div>Loading...</div>}
+        {mutation.isError && <p>{mutation.error.message}</p>}
+        <Link to="/">
+          <FloatingActionButton>
+            <img src={BackArrow} alt="back" />
+          </FloatingActionButton>
+        </Link>
+      </Wrapper>
+    </>
   );
 };
 export default Add;
