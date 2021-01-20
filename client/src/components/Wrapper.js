b3fa01fc3dc0c2d98f33b76511112021bae68999
kschmitz123 <kathrin_schmitz93@gmx.de>
2021-01-20 09:59:00 +0100

    Change styling of wrapper

--- client/src/components/Wrapper.js
+++ client/src/components/Wrapper.js
@@ -3,6 +3,7 @@ import styled from "styled-components/macro";
 const Wrapper = styled.div`
   padding: 30px;
   text-align: center;
-  height: 100vh; ;
+  height: 100vh;
+  background-color: #e4e2dd;
 `;
 export default Wrapper;
