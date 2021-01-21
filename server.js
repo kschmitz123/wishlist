89bd31f5851928a4a828355d8f2f2f19005ead7e
kschmitz123 <kathrin_schmitz93@gmx.de>
2021-01-21 12:40:35 +0100

    Outsource routes

--- server.js
+++ server.js
@@ -2,41 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const { connect } = require("./lib/database");
-const {
-  setWishlist,
-  getWishlistById,
-  updateWishlist,
-  getWishlists,
-  deleteWishlistById,
-} = require("./lib/wishlists");
-
+const lists = require("./lib/routes/lists");
+const { updateWishlist } = require("./lib/models/wishlists");
 const app = express();
 const port = process.env.PORT || 3001;
 app.use(express.static("public"));
 app.use(express.json());
 
-app.post("/api/lists/", async (request, response) => {
-  const wishlist = request.body;
-  try {
-    const insertResult = await setWishlist(wishlist);
-    const newWishlistId = insertResult.insertedId;
-    response.status(200).json(newWishlistId);
-  } catch (error) {
-    console.error(error);
-    response.status(500).send("An internal server error occured");
-  }
-});
-
-app.get("/api/lists/", async (request, response) => {
-  const { lists } = request.params;
-  try {
-    const listCollection = await getWishlists(lists);
-    response.send(listCollection);
-  } catch (error) {
-    console.error(error);
-    response.status(500).send("An internal server error occured");
-  }
-});
+app.use("/api/lists", lists);
 
 app.post("/api/wishlist/", async (request, response) => {
   const data = request.body;
@@ -44,28 +17,6 @@ app.post("/api/wishlist/", async (request, response) => {
     const insertResult = await updateWishlist(data);
     const newWishlistId = insertResult.insertedId;
     response.status(200).json(newWishlistId);
-  } catch (error) {
-    console.error(error);
-    response.status(500).send("An internal server error occured");
-  }
-});
-
-app.get("/api/lists/:id", async (request, response) => {
-  const { id } = request.params;
-  try {
-    const wishlist = await getWishlistById(id);
-    response.status(200).send(wishlist);
-  } catch (error) {
-    console.error(error);
-    response.status(500).send("An internal server error occured");
-  }
-});
-
-app.delete("/api/lists/:id", async (request, response) => {
-  const { id } = request.params;
-  try {
-    await deleteWishlistById(id);
-    response.status(200).send();
   } catch (error) {
     console.error(error);
     response.status(500).send("An internal server error occured");
