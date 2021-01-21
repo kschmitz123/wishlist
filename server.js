0cd6fbc20dc6d85189cd108a063f9e604b231738
kschmitz123 <kathrin_schmitz93@gmx.de>
2021-01-21 12:44:35 +0100

    Outsource update route

--- server.js
+++ server.js
@@ -3,25 +3,14 @@ const express = require("express");
 const path = require("path");
 const { connect } = require("./lib/database");
 const lists = require("./lib/routes/lists");
-const { updateWishlist } = require("./lib/models/wishlists");
+const wishlist = require("./lib/routes/wishlist");
 const app = express();
 const port = process.env.PORT || 3001;
 app.use(express.static("public"));
 app.use(express.json());
 
 app.use("/api/lists", lists);
-
-app.post("/api/wishlist/", async (request, response) => {
-  const data = request.body;
-  try {
-    const insertResult = await updateWishlist(data);
-    const newWishlistId = insertResult.insertedId;
-    response.status(200).json(newWishlistId);
-  } catch (error) {
-    console.error(error);
-    response.status(500).send("An internal server error occured");
-  }
-});
+app.use("/api/wishlist", wishlist);
 
 app.use(express.static(path.join(__dirname, "client/build")));
 // app.use(
